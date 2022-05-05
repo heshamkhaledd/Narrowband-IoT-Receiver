4e9aadb4bc0271fc97bbdba5036cbc32200e5878
heshamkhaledd <heshamkhaled13@gmail.com>
2022-05-05 21:24:22 +0200

    ⚙️: fixed cp removal bug

--- sw_model/cp_removal_downsampler/cp_removal_downsampler.m
+++ sw_model/cp_removal_downsampler/cp_removal_downsampler.m
@@ -9,9 +9,7 @@ close all; clear; clc; rng('shuffle');
 %   -> IFFT Points:     128
 
 N_SubFrames = 10; % Number of required Narrowband LTE Frames to be generated
-Nf = 19200;
 Ns = 128;
-Nw = 19200;
 LTE_preset = 'R.NB.6';
 LTE_Settings = NBIoTDownlinkWaveformGenerator(LTE_preset);  
 LTE_Settings.Config.TotSubframes = N_SubFrames;
@@ -22,19 +20,22 @@ LTE_Settings.Config.NPDCCH.NRep = 4;
 LTE_Settings.Config.SIB1NPDSCH.Enable = 'Off';
 LTE_Settings.Config.OperationMode = 'GuardBand';
 [NB_Frames,NB_Symbols,NB_Info] = LTE_Settings.generateWaveform;
-% displayResourceGrid(LTE_Settings);
+displayResourceGrid(LTE_Settings);
 %% CP Removal %%
-CPR_NB_Frames = zeros(Ns*14*N_SubFrames,1);
+CPR_NB_Frames = zeros(14*Ns*N_SubFrames,1);
+Slice_Start = zeros(14,1);
+
+Slice_Start(1,1) = 11;
+for Idx = 2 : 1 : 14
+    Slice_Start(Idx,1) = Slice_Start(Idx-1,1) + 128 + NB_Info.CyclicPrefixLengths(1,Idx);
+end
+
 % 1st & 8th Symbols have CP 10 samples, and the rest have 9 samples
-for Idx = 1 : 1920 : 1920*N_SubFrames
-    for Itr = Idx : 137 : Idx+1920
-        if (Itr == Idx)
-            CPR_NB_Frames(Itr+10:Itr+137,1) = NB_Frames(Itr+10:Itr+137,1);
-        elseif(Itr == Idx+959)
-            CPR_NB_Frames(Itr+11:Itr+138,1) = NB_Frames(Itr+11:Itr+138,1);   
-        else
-            CPR_NB_Frames(Itr+10:Itr+137,1) = NB_Frames(Itr+10:Itr+137,1);
-        end
+counter = 1;
+for Idx = 1 : 1920 : size(NB_Frames,1)
+    for Itr = 1:size(Slice_Start,1)
+        CPR_NB_Frames(counter:counter+127,1) = NB_Frames(Slice_Start(Itr,1)+Idx-1:Slice_Start(Itr,1)+126+Idx,1);
+        counter = counter + 128;
     end
 end
 %% Downsampling %%
@@ -44,4 +45,8 @@ CPR_DS_NB_Frames = zeros(size(CPR_NB_Frames,1)/8,1);
         CPR_DS_NB_Frames(counter,1) = NB_Frames(Idx,1);
         counter = counter + 1;
     end
+output_fx = round(CPR_DS_NB_Frames .*2^10);
 %% Exporting input to Verilog Testbench
+input_fx(:,1) = round(real(NB_Frames) .* 2^10);
+input_fx(:,2) = round(imag(NB_Frames) .* 2^10);
+dlmwrite('../../src/cprds/cprds_input.txt', input_fx, ' ');
